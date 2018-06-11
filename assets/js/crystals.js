b26be6bc54356d2a2dc5c94c2f9e329c47782e65
Manuel Savino <msavino@fiu.edu>
2018-06-11 09:56:22 -0400

    fixed win losses being called too early

--- assets/js/crystals.js
+++ assets/js/crystals.js
@@ -1,6 +1,6 @@
 $(function () {
 
-    var totalScore = 0;
+    var userScore = 0;
     var wins = 0;
     var losses = 0;
 
@@ -16,72 +16,85 @@ $(function () {
     $("#losses").html(losses)
 
     $("#crystal1").on("click", function () {
-        totalScore = totalScore + number1;
-        $("#playerScore").html(totalScore)
-
-        if (totalScore == scoreNeeded) {
+        userScore = userScore + number1;
+        $("#playerScore").html(userScore)
+        $("#crystal1Num").text(number1)
+        if (userScore == scoreNeeded) {
             userWin();
-        } else if (totalScore > scoreNeeded) {
+        } else if (userScore > scoreNeeded) {
             userLoss();
         }
     })
 
     $("#crystal2").on("click", function () {
-        totalScore = totalScore + number2;
-        $("#playerScore").html(totalScore)
+        userScore = userScore + number2;
+        $("#playerScore").html(userScore)
+        $("#crystal2Num").text(number2)
 
-        if (totalScore == scoreNeeded) {
+        if (userScore == scoreNeeded) {
             userWin();
-        } else if (totalScore > scoreNeeded) {
+        } else if (userScore > scoreNeeded) {
             userLoss();
         }
     })
 
     $("#crystal3").on("click", function () {
-        totalScore = totalScore + number3;
-        $("#playerScore").html(totalScore)
+        userScore = userScore + number3;
+        $("#playerScore").html(userScore)
+        $("#crystal3Num").text(number3)
 
-        if (totalScore == scoreNeeded) {
+        if (userScore == scoreNeeded) {
             userWin();
-        } else if (totalScore > scoreNeeded) {
+        } else if (userScore > scoreNeeded) {
             userLoss();
         }
     })
 
     $("#crystal4").on("click", function () {
-        totalScore = totalScore + number4;
-        $("#playerScore").html(totalScore)
+        userScore = userScore + number4;
+        $("#playerScore").html(userScore)
+        $("#crystal4Num").text(number4)
 
-        if (totalScore == scoreNeeded) {
+        if (userScore == scoreNeeded) {
             userWin();
-        } else if (totalScore > scoreNeeded) {
+        } else if (userScore > scoreNeeded) {
             userLoss();
         }
+    })
+
+    $("#newGame").on("click", function(){
+        reset()
     })
 
     function userWin() {
         alert("You won!");
         wins++;
         $("#wins").html(wins);
-        reset();
+
     }
 
     function userLoss() {
-        alert("You lost!");
+        setTimeout(() => {
+            alert("You lost!");
+        }, 500);
+        
         losses++;
         $("#losses").html(losses);
-        reset();
     }
 
     function reset() {
-        var totalScore = 0;
-        var scoreNeeded = Math.floor(Math.random() * 102 + 19);
+        userScore = 0;
+        scoreNeeded = Math.floor(Math.random() * 102 + 19);
         $("#targetScore").html(scoreNeeded)
-        var number1 = Math.floor(Math.random() * 12 + 1);
-        var number2 = Math.floor(Math.random() * 12 + 1);
-        var number3 = Math.floor(Math.random() * 12 + 1);
-        var number4 = Math.floor(Math.random() * 12 + 1);
-        $("#playerScore").html(totalScore)
+        number1 = Math.floor(Math.random() * 12 + 1);
+        number2 = Math.floor(Math.random() * 12 + 1);
+        number3 = Math.floor(Math.random() * 12 + 1);
+        number4 = Math.floor(Math.random() * 12 + 1);
+        $("#crystal1Num").text("")
+        $("#crystal2Num").text("")
+        $("#crystal3Num").text("")
+        $("#crystal4Num").text("")
+        $("#playerScore").html(userScore)
     }
 
 })
